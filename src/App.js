d68388f9675067a95e16e762946f86b908929cbd
iosilva <iosilva@I4PRO08>
2019-12-26 13:23:12 -0300

    Testando GSAP no projeto!

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import GlobalStyle from "./styles/global";
 
 import Header from "./components/Header";
 import Content from "./components/Content";
+import Loader from "./components/Loader";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Loader />
       <Content />
       <Footer />
 
